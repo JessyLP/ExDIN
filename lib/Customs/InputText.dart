0224250e0dbf6d4b0e077a2abc90705e24cbda43
alumno <alumno@gmail.com>
2022-11-16 13:05:12 +0100

    Clase inputText y login , con contraseña y usuario

---
+++ lib/Customs/InputText.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class InputText extends StatelessWidget {
+
+  final String titulo;
+  final String ayuda;
+  final bool blIsPsswd;
+
+
+
+  InputText({Key? key,  this.titulo="",  this.ayuda="",  this.blIsPsswd=false}) : super(key:key);
+  final TextEditingController myController = TextEditingController();
+
+
+  String getText(){
+    return myController.text;
+  }
+
+  void setInitialValue(String initialValue){
+    myController.text=initialValue;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      obscureText: blIsPsswd,
+      controller: myController,
+      cursorColor: Colors.deepPurple,
+      enableSuggestions: !blIsPsswd,
+      autocorrect: !blIsPsswd,
+      decoration:  InputDecoration(
+        icon: Icon(Icons.account_circle_outlined),
+        labelText: titulo,
+        labelStyle: TextStyle(
+          color: Color(0xFF6B11A4),
+        ),
+        helperText: ayuda,
+        suffixIcon: Icon(
+          Icons.check_circle,
+        ),
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Color(0xFF6200EE)),
+        ),
+      ),
+    );
+  }
+
+}
---
+++ lib/Views/LoginView.dart
@@ -0,0 +1,119 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../Customs/InputText.dart';
+
+
+class loginView extends StatelessWidget{
+
+  loginView({Key? key}) : super(key:key);
+
+
+  InputText inputUser = InputText(
+    titulo: "Correo:",
+    ayuda:"ejemplo@gmail.com",);
+  InputText inputPassword = InputText(
+    titulo: "Password:",
+    ayuda:"Mayor de 7 caracteres",
+    blIsPsswd: true,);
+
+
+  void btn1Pressed(BuildContext context) async{
+    /* print(input1.geText());
+     print(input2.geText());*/
+    try {
+      final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: inputUser.getText(),
+        password: inputPassword.getText(),
+      );Navigator.of(context).popAndPushNamed("/OnBoarding");
+    } on FirebaseAuthException catch (e) {
+      print("------------> ERROR AL CREAR EL USUARIO.");
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+    print("USUARIO CREADO CORRECTAMENTE");
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return MaterialApp(
+        home: Container(
+          child: Scaffold(
+
+            appBar: AppBar(
+              title: const Text(' LOGIN  '),
+              titleTextStyle: TextStyle(fontWeight: FontWeight.bold ),
+              backgroundColor: Colors.deepPurple,
+            ),
+            body:
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  inputUser,
+                  inputPassword,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children:  [
+                      ElevatedButton(
+                        onPressed: () async {
+                          try {
+                            final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+                              email: inputUser.getText(),
+                              password: inputPassword.getText(),
+                            );Navigator.of(context).popAndPushNamed("/Home");
+                          } on FirebaseAuthException catch (e) {
+                            print("------------> ERROR AL ENTRAR .");
+                            if (e.code == 'user-not-found') {
+                              print('No user found dor that email.');
+                            } else if (e.code == 'wrong-password') {
+                              print('Wrong password provided for that user');
+                            }
+                          } catch (e) {
+                            print(e);
+                          };
+                        },child: Text(' LOGIN'),
+                        style:ElevatedButton.styleFrom(
+                            backgroundColor: Colors.indigo
+                        ),
+
+                      ),
+
+
+                      ElevatedButton.icon(
+                        onPressed: (){
+                          Navigator.of(context).popAndPushNamed("/registerview");
+                        },
+                        icon: Icon(Icons.accessibility, size: 18),
+                        label: Text(' REGISTRAR'),
+                        style:ElevatedButton.styleFrom(
+                            backgroundColor: Colors.purple
+                        ),
+
+                      ),
+
+
+                    ],
+                  ),
+                ],
+              ),
+
+            ),
+            backgroundColor: Colors.cyan,
+
+          ),
+        )
+    );
+
+  }
+
+}
