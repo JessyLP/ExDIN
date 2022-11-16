316c8f017de2384568942bc32f2738aa004f4e2a
jessica <dam.jessica.lopez@gmail.com>
2022-11-16 18:26:24 +0100

    Clase platform

---
+++ lib/Views/Plataforma/Platform.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+
+class PlatformAdmin {
+
+  //int iANDROID_PLATFORM=0;
+  //int iIOS_PLATFORM=1;
+  //int iWEB_PLATFORM=2;
+  double SCREEN_WIDTH = 0;
+  double SCREEN_HEIGHT = 0;
+  late BuildContext context;
+
+  PlatformAdmin();
+
+  double getScreenWidth(BuildContext context){
+    SCREEN_WIDTH=MediaQuery.of(context).size.width;
+    return SCREEN_WIDTH;
+  }
+
+  double getScreenHeight(BuildContext context){
+    SCREEN_HEIGHT=MediaQuery.of(context).size.height;
+    return SCREEN_HEIGHT;
+  }
+
+}
