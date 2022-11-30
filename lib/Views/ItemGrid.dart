f260a8830e36a3975e3dda0d4e6be348b4f260ee
alumno <alumno@gmail.com>
2022-11-30 12:30:18 +0100

    mod

--- lib/Views/ItemGrid.dart
+++ lib/Views/ItemGrid.dart
@@ -32,6 +32,9 @@ class _ItemGridState extends State<ItemGrid>{
                   ListView(
                     padding: const EdgeInsets.all(10),
                     children: <Widget>[
+                      Divider(color: Colors.blue,height: 2,thickness: 2),
+                      Divider(color: Colors.blue,height: 2,thickness: 2),
+
                       Container(
                         height: 50,
                         decoration: BoxDecoration(
@@ -41,7 +44,28 @@ class _ItemGridState extends State<ItemGrid>{
                         ),
                         child: const Center(child: Text('Pescado')),
                       ),
-                      Text(""),
+
+                      Divider(color: Colors.blue,height: 2,thickness: 2),
+                      Divider(color: Colors.blue,height: 2,thickness: 2),
+
+                      Container(
+                        child: Column(
+                          children: [
+                            Text("\nRape\n",style: TextStyle(color: Colors.black)),
+                            Text("Este pescado también es conocido con el nombre de sapo o pejesapo. Se trata de un pescado blanco de agua salada que vive en el fondo oceánico. Gracias a la consistencia de su carne, y como sus filetes no tienen espinas, resulta perfecto para los que sienten rechazo al pescado."),
+
+                            Text("\nGallo\n",style: TextStyle(color: Colors.black)),
+                            Text("Se trata de un pescado cuya característica principal es que tiene ambos ojos situados en el lado izquierdo del cuerpo. El Gallo es un pescado blanco de textura y sabor suaves con muy bajo contenido en grasa y hay muchas recetas en las que es el protagonista."),
+
+                            Text("\n Bacalao\n",style: TextStyle(color: Colors.black)),
+                            Text("El Bacalao es un pescado blanco de agua salada del que existen unas 60 especies distintas, pero el más conocido y de mayor importancia comercial es el bacalao común. Se trata de un pescado de sabor agradable, de hecho más destacado que el de otros pescados blancos.\n"),
+                          ],
+                        ),
+                      ),
+                      Divider(color: Colors.blue,height: 2,thickness: 2),
+                      Divider(color: Colors.blue,height: 2,thickness: 2),
+
+
                       Container(
                         height: 50,
                         decoration: BoxDecoration(
@@ -49,9 +73,31 @@ class _ItemGridState extends State<ItemGrid>{
                               image: AssetImage('assets/images/carne.jpg'),
                               fit: BoxFit.cover),
                         ),
-                        child: const Center(child: Text('Carne')),
+                        child: const Center(child: Text('Carne',style: TextStyle(color: Colors.white))),
                       ),
-                      Text(""),
+
+                      Divider(color: Colors.blue,height: 2,thickness: 2),
+                      Divider(color: Colors.blue,height: 2,thickness: 2),
+
+                      Container(
+                        child: Column(
+                          children: [
+                            Text("\nDe vaca, res, buey o bien ternera (bóvida)\n",style: TextStyle(color: Colors.black)),
+                            Text("Las hamburguesas, las costillas o bien los filetes de vaca y ternera son seguramente los ejemplos más representativos de la carne roja."),
+
+                            Text("\nDe cerdo (porcina)\n",style: TextStyle(color: Colors.black)),
+                            Text("La carne de cerdo se emplea para preparar comestibles como el jamón, las salchichas, el bacón o bien el prosciutto. Casi todas las unas partes del cuerpo de los cerdos son susceptibles de utilizarse como comestible, desde la cabeza y la papada hasta las extremidades, el rabo o bien las gónadas."),
+
+                            Text("\n De oveja y cordero (ovina)\n",style: TextStyle(color: Colors.black)),
+                            Text("La carne ovina es considerada uno de las clases más saludables. En esta concepción influyen las altas concentraciones de nutrientes relevantes para las defensas como las vitaminas B, el selenio y el cinc, como su proporción inmejorable entre ácidos grasos omega-tres y seis. Sin embargo, el cordero es costoso en comparación con otras carnes rojas \n"),
+                          ],
+                        ),
+                      ),
+                      Divider(color: Colors.blue,height: 2,thickness: 2),
+                      Divider(color: Colors.blue,height: 2,thickness: 2),
+
+
+
                       Container(
                         height: 50,
                         decoration: BoxDecoration(
@@ -61,7 +107,22 @@ class _ItemGridState extends State<ItemGrid>{
                         ),
                         child: const Center(child: Text('Vegetales')),
                       ),
-                    ],
+
+                      Divider(color: Colors.blue,height: 2,thickness: 2),
+                      Divider(color: Colors.blue,height: 2,thickness: 2),
+
+                      Container(
+                        child: Column(
+                          children: [
+                            Text("\n Legumbres\n",style: TextStyle(color: Colors.black)),
+                            Text("Que podemos comerlas estando verdes o frescas, como lo son los guisantes, por poner un ejemplo."),
+
+                            Text("\n Bulbos\n",style: TextStyle(color: Colors.black)),
+                            Text("Verduras bastante carnosas pero no nacen de las flores, como lo son el ajo, o cualquier otra verdura que se incluya en esta categoría.\n"),
+                          ],
+                        ),
+                      ),
+                      ],
                   )
 
         )
